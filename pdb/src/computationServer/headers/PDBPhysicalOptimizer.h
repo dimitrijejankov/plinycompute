be58f9fc67d410127cf8ed84ea3ef91b0c0398c6
Dimitrije Jankov <dimitrijejankov@gmail.com>
2019-02-21 07:37:23 -0600

    Started working on the physical optimizer

---
+++ pdb/src/computationServer/headers/PDBPhysicalOptimizer.h
@@ -0,0 +1,49 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+#ifndef PDB_PHYSICAL_OPTIMIZER_H
+#define PDB_PHYSICAL_OPTIMIZER_H
+
+
+#include "Computation.h"
+#include "PDBVector.h"
+#include <PDBLogger.h>
+#include <AtomicComputationList.h>
+
+namespace pdb {
+
+class PDBPhysicalOptimizer {
+public:
+
+  PDBPhysicalOptimizer(String tcapString, PDBLoggerPtr &logger);
+
+  ~PDBPhysicalOptimizer() = default;
+
+ private:
+
+  std::unique_ptr<AtomicComputationList> atomicComputations;
+
+  std::map<size_t, AtomicComputationPtr>
+
+  PDBLoggerPtr logger;
+
+};
+
+}
+
+
+#endif //PDB_PHYSICAL_OPTIMIZER_H
---
+++ pdb/src/computationServer/sources/PDBPhysicalOptimizer.cc
@@ -0,0 +1,41 @@
+//
+// Created by dimitrije on 2/21/19.
+//
+
+#include <PDBPhysicalOptimizer.h>
+#include <AtomicComputationList.h>
+#include <Lexer.h>
+#include <Parser.h>
+
+namespace pdb {
+
+PDBPhysicalOptimizer::PDBPhysicalOptimizer(String tcapString, PDBLoggerPtr &logger) : logger(logger) {
+
+  // get the string to compile
+  std::string myLogicalPlan = tcapString;
+  myLogicalPlan.push_back('\0');
+
+  // where the result of the parse goes
+  AtomicComputationList *myResult;
+
+  // now, do the compilation
+  yyscan_t scanner;
+  LexerExtra extra{""};
+  yylex_init_extra(&extra, &scanner);
+  const YY_BUFFER_STATE buffer{yy_scan_string(myLogicalPlan.data(), scanner)};
+  const int parseFailed{yyparse(scanner, &myResult)};
+  yy_delete_buffer(buffer, scanner);
+  yylex_destroy(scanner);
+
+  // if it didn't parse, get outta here
+  if (parseFailed) {
+    std::cout << "Parse error when compiling TCAP: " << extra.errorMessage;
+    exit(1);
+  }
+
+  // this is the logical plan to return
+  atomicComputations = std::unique_ptr<AtomicComputationList>(myResult);
+}
+
+}
+
