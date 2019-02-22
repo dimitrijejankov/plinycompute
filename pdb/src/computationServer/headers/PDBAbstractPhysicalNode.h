15291d91f89bd4783cc50eef9945c9075b39fcfe
Dimitrije Jankov <dimitrijejankov@gmail.com>
2019-02-22 04:33:52 -0600

    Did some work on the node

--- pdb/src/computationServer/headers/PDBAbstractPhysicalNode.h
+++ pdb/src/computationServer/headers/PDBAbstractPhysicalNode.h
@@ -19,10 +19,8 @@ enum PDBPipelineType {
 namespace pdb {
 
 class PDBAbstractPhysicalNode;
-
-
-typedef std::shared_ptr<PDBAbstractPhysicalNode> PDBAbstractPhysicalNodePtr;
-typedef std::shared_ptr<PDBAbstractPhysicalNode> PDBAbstractPhysicalNodeWeakPtr;
+using PDBAbstractPhysicalNodePtr = std::shared_ptr<PDBAbstractPhysicalNode>;
+using PDBAbstractPhysicalNodeWeakPtr = std::weak_ptr<PDBAbstractPhysicalNode>;
 
 class PDBAbstractPhysicalNode {
 
@@ -34,39 +32,11 @@ public:
   virtual ~PDBAbstractPhysicalNode() = default;
 
   /**
-   * Where the pipeline begins
+   * Returns a shared pointer handle to this node
+   * @return the shared pointer handle
    */
-  std::vector<AtomicComputationPtr> pipeline;
-
-  /**
-   * A list of consumers of this node
-   */
-  std::list<PDBAbstractPhysicalNodePtr> consumers;
-
-  /**
-   * A list of producers of this node
-   */
-  std::list<PDBAbstractPhysicalNodeWeakPtr> producers;
-
-  /**
-   * A shared pointer to an instance of this node
-   */
-  PDBAbstractPhysicalNodeWeakPtr handle;
-
-  bool hasScanSet() { return false;};
-
-  /**
- * Returns a shared pointer handle to this node
- * @return the shared pointer handle
- */
   PDBAbstractPhysicalNodePtr getHandle() {
-
-    // if we do not have a handle to this node already
-    if(handle == nullptr) {
-      handle = std::shared_ptr<PDBAbstractPhysicalNode> (this);
-    }
-
-    return handle;
+    return getWeakHandle().lock();
   }
 
   /**
@@ -74,8 +44,10 @@ public:
    * @param consumer the consumer we want to remove
    */
   virtual void removeConsumer(const PDBAbstractPhysicalNodePtr &consumer) {
+
+    // detach them
     consumers.remove(consumer);
-    consumer->producers.remove(getHandle());
+    consumer->producers.remove_if([&](PDBAbstractPhysicalNodeWeakPtr p){ return this->getHandle() == p.lock(); });
   }
 
   /**
@@ -84,7 +56,7 @@ public:
   */
   virtual void addConsumer(const pdb::PDBAbstractPhysicalNodePtr &consumer) {
     consumers.push_back(consumer);
-    consumer->producers.push_back(getHandle());
+    consumer->producers.push_back(getWeakHandle());
   }
 
   /**
@@ -103,7 +75,53 @@ public:
 
   virtual std::string getNodeIdentifier() = 0;
 
-  virtual std::vector<AtomicComputationPtr>& getPipeComputations() { return pipeline; }
+  /**
+   * Returns all the atomic computations the make up this pipeline
+   * @return a vector with the atomic computations
+   */
+  const std::vector<AtomicComputationPtr>& getPipeComputations() { return pipeline; }
+
+  /**
+   * // TODO remove this somehow
+   * @return
+   */
+  bool hasScanSet() { return false;};
+
+private:
+
+  /**
+   * Returns a weak pointer handle to this node
+   * @return the weak pointer handle
+   */
+  PDBAbstractPhysicalNodeWeakPtr getWeakHandle() {
+
+    // if we do not have a handle to this node already
+    if(handle.expired()) {
+      handle = std::shared_ptr<PDBAbstractPhysicalNode> (this);
+    }
+
+    return handle;
+  }
+
+  /**
+ * Where the pipeline begins
+ */
+  std::vector<AtomicComputationPtr> pipeline;
+
+  /**
+   * A list of consumers of this node
+   */
+  std::list<PDBAbstractPhysicalNodePtr> consumers;
+
+  /**
+   * A list of producers of this node
+   */
+  std::list<PDBAbstractPhysicalNodeWeakPtr> producers;
+
+  /**
+   * A shared pointer to an instance of this node
+   */
+  PDBAbstractPhysicalNodeWeakPtr handle;
 };
 
 }
