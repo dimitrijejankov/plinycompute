a3baf375ed3a81121c010bc31c043f2721ffd673
Vicram Rajagopalan <vicram.r@hotmail.com>
2019-03-24 20:16:00 -0500

    Started working on implementing distributed Aggregation. Copied CombinedShuffleSink.h from master PlinyCompute repo. All of its dependencies were present (and looked unchanged), except for DataTypes.h; however, I commented out `#include "DataTypes.h"` and it didn't cause any errors in CLion.
    Wrote signatures for the methods we're overriding in AggregationPipeAlgorithm.

--- pdb/src/executionServer/headers/physicalAlgorithms/PDBAggregationPipeAlgorithm.h
+++ pdb/src/executionServer/headers/physicalAlgorithms/PDBAggregationPipeAlgorithm.h
@@ -8,17 +8,50 @@
 // PRELOAD %PDBAggregationPipeAlgorithm%
 
 #include "PDBPhysicalAlgorithm.h"
+#include "Pipeline.h"
+#include <vector>
 
 namespace pdb {
 
 class PDBAggregationPipeAlgorithm : public PDBPhysicalAlgorithm {
 public:
 
+  ENABLE_DEEP_COPY
+
   PDBAggregationPipeAlgorithm() = default;
 
   PDBAggregationPipeAlgorithm(const Handle<PDBSourcePageSetSpec> &source,
                               const Handle<PDBSinkPageSetSpec> &sink,
                               const Handle<Vector<PDBSourcePageSetSpec>> &secondarySources);
+
+  bool setup(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage, Handle<pdb::ExJob> &job, const std::string &error) override;
+
+  bool run(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage) override;
+
+  PDBPhysicalAlgorithmType getAlgorithmType() override;
+
+ private:
+  /**
+   * Vector of pipelines that will run the pre-aggregation portion of this algorithm.
+   * These will create the hashmaps which are sent to other worker nodes.
+   * The pipelines will be built when you call setup on this object.
+   * This must be null when sending this object.
+   */
+  std::shared_ptr<std::vector<PipelinePtr>> preAggPipelines = nullptr;
+
+  /**
+   * Vector of pipelines that will run the final aggregation portion of this algorithm.
+   * These will receive the hashmaps from the pre-aggregation and finish the aggregation
+   * locally. The pipelines will be built when you call setup on this object.
+   * This must be null when sending this object.
+   */
+  std::shared_ptr<std::vector<PipelinePtr>> finalAggPipelines = nullptr;
+
+
+  /**
+   * Should we materialize the output of the final aggregation, or not?
+   */
+  bool shouldMaterialize = false;
 
 };
 
--- pdb/src/executionServer/sources/physicalAlgorithms/PDBAggregationPipeAlgorithm.cc
+++ pdb/src/executionServer/sources/physicalAlgorithms/PDBAggregationPipeAlgorithm.cc
@@ -7,3 +7,17 @@
 pdb::PDBAggregationPipeAlgorithm::PDBAggregationPipeAlgorithm(const pdb::Handle<pdb::PDBSourcePageSetSpec> &source,
                                                               const pdb::Handle<pdb::PDBSinkPageSetSpec> &sink,
                                                               const pdb::Handle<pdb::Vector<pdb::PDBSourcePageSetSpec>> &secondarySources) : PDBPhysicalAlgorithm(source, sink, secondarySources) {}
+
+bool pdb::PDBAggregationPipeAlgorithm::setup(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage,
+                                             Handle<pdb::ExJob> &job,
+                                             const std::string &error) {
+
+}
+
+bool pdb::PDBAggregationPipeAlgorithm::run(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage) {
+
+}
+
+pdb::PDBPhysicalAlgorithmType pdb::PDBAggregationPipeAlgorithm::getAlgorithmType() {
+  return DistributedAggregation;
+}
