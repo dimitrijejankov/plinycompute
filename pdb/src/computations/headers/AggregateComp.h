fef96fab8196113a088df22addea806b0088a10f
Vicram Rajagopalan <vicram.r@hotmail.com>
2019-03-26 02:17:46 -0500

    Added AggregateCompBase.h. We need at least 2 levels of inheritance for the AggregateComp. This is because the only thing we want to expose to the programmer is the two lambda methods which must be defined, but the system needs to have a bunch of other methods it can use. AggregateComp.h is now basically exactly the same as AggregateComp.h from the master branch. I just copied the methods that were formerly in AggregateComp and pasted those into AggregateCompBase.h, because that's where they should be (hidden from the user). For some reason, CLion is saying that it can't find the symbol "AggregateCompBase" even though I remembered to #include AggregateCompBase.h.

--- pdb/src/computations/headers/AggregateComp.h
+++ pdb/src/computations/headers/AggregateComp.h
@@ -23,6 +23,7 @@
 #include "HashSink.h"
 #include "MapTupleSetIterator.h"
 #include "DepartmentTotal.h"
+#include "AggregateCompBase.h"
 
 namespace pdb {
 
@@ -38,97 +39,25 @@ namespace pdb {
 // and the result of getValue () is set to the desired value.
 //
 template<class OutputClass, class InputClass, class KeyClass, class ValueClass>
-class AggregateComp : public Computation {
-
-  // gets the operation tht extracts a key from an input object
-  virtual Lambda<KeyClass> getKeyProjection(Handle<InputClass> aggMe) = 0;
-
-  // gets the operation that extracts a value from an input object
-  virtual Lambda<ValueClass> getValueProjection(Handle<InputClass> aggMe) = 0;
-
-  // extract the key projection and value projection
-  void extractLambdas(std::map<std::string, LambdaObjectPtr> &returnVal) override {
-    int suffix = 0;
-    Handle<InputClass> checkMe = nullptr;
-    Lambda<KeyClass> keyLambda = getKeyProjection(checkMe);
-    Lambda<ValueClass> valueLambda = getValueProjection(checkMe);
-    keyLambda.toMap(returnVal, suffix);
-    valueLambda.toMap(returnVal, suffix);
-  }
-
-  // this is an aggregation comp
-  std::string getComputationType() override {
-    return std::string("AggregationComp");
-  }
-
-  int getNumInputs() override {
-    return 1;
-  }
-
-  // gets the name of the i^th input type...
-  std::string getIthInputType(int i) override {
-    if (i == 0) {
-      return getTypeName<InputClass>();
-    } else {
-      return "";
-    }
-  }
-
-  // gets the output type of this query as a string
-  std::string getOutputType() override {
-    return getTypeName<OutputClass>();
-  }
-
-  // below function implements the interface for parsing computation into a TCAP string
-  std::string toTCAPString(std::vector<InputTupleSetSpecifier> inputTupleSets,
-                           int computationLabel,
-                           std::string &outputTupleSetName,
-                           std::vector<std::string> &outputColumnNames,
-                           std::string &addedOutputColumnName) override {
-
-    if (inputTupleSets.empty()) {
-      return "";
-    }
-
-    InputTupleSetSpecifier inputTupleSet = inputTupleSets[0];
-    return toTCAPString(inputTupleSet.getTupleSetName(),
-                        inputTupleSet.getColumnNamesToKeep(),
-                        inputTupleSet.getColumnNamesToApply(),
-                        computationLabel,
-                        outputTupleSetName,
-                        outputColumnNames,
-                        addedOutputColumnName);
-  }
-
-  // to return Aggregate tcap string
-  std::string toTCAPString(std::string inputTupleSetName,
-                           std::vector<std::string> inputColumnNames,
-                           std::vector<std::string> inputColumnsToApply,
-                           int computationLabel,
-                           std::string &outputTupleSetName,
-                           std::vector<std::string> &outputColumnNames,
-                           std::string &addedOutputColumnName) {
-    return "";
-  }
+class AggregateComp : public AggregateCompBase {
 
   /**
-   * Return the hash sink for the aggregation
-   * @param consumeMe -
-   * @param projection
-   * @param plan
-   * @return
+   * Gets the operation that extracts a key from an input object
+   * @param aggMe - the object we want to get the operation from
+   * @return the projection lambda. This must be a lambda which takes in
+   * a Handle<InputClass> and constructs an object of type KeyClass.
    */
-  ComputeSinkPtr getComputeSink(TupleSpec &consumeMe, TupleSpec &projection) override {
-    return std::make_shared<pdb::HashSink<KeyClass, ValueClass>>(consumeMe, projection);
-  }
+  virtual Lambda<KeyClass> getKeyProjection(Handle<InputClass> aggMe) = 0;
 
-  ComputeSourcePtr getComputeSource(const PDBAbstractPageSetPtr &pageSet, size_t chunkSize, uint64_t workerID) override {
-    return std::make_shared<MapTupleSetIterator<KeyClass, ValueClass, OutputClass>> (pageSet, workerID, chunkSize);
-  }
+  /**
+   * Gets the operation that extracts a value from an input object
+   * @param aggMe - the object we want to get the operation from
+   * @return the projection lambda. This must be a lambda which takes in
+   * a Handle<InputClass> and constructs an object of type ValueClass.
+   */
+  virtual Lambda<ValueClass> getValueProjection(Handle<InputClass> aggMe) = 0;
 
-  bool needsMaterializeOutput() override {
-    return false;
-  }
+
 
 };
 
