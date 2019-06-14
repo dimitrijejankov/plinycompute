03beacae4204f4ae7cf8ff3efe501db5d5ba5221
Yuxin Tang <yuxin.tang@rice.edu>
2019-06-14 18:14:15 -0500

    Complete the implementation for BroadcastJoin Physical Algorithm

--- pdb/src/executionServer/headers/physicalAlgorithms/PDBBroadcastForJoinAlgorithm.h
+++ pdb/src/executionServer/headers/physicalAlgorithms/PDBBroadcastForJoinAlgorithm.h
@@ -16,17 +16,17 @@
 namespace pdb {
 
 class PDBBroadcastForJoinAlgorithm : public PDBPhysicalAlgorithm {
-public:
+ public:
 
   PDBBroadcastForJoinAlgorithm() = default;
 
   PDBBroadcastForJoinAlgorithm(const std::string &firstTupleSet,
                                const std::string &finalTupleSet,
-                               const pdb::Handle<PDBSourcePageSetSpec> &source,
-                               const pdb::Handle<PDBSinkPageSetSpec> &intermediate,
-                               const pdb::Handle<PDBSinkPageSetSpec> &sink,
-                               const pdb::Handle<pdb::Vector<pdb::Handle<PDBSourcePageSetSpec>>> &secondarySources,
-                               bool swapLHSandRHS);
+                               const pdb::Handle<pdb::PDBSourcePageSetSpec> &source,
+                               const pdb::Handle<pdb::PDBSinkPageSetSpec> &intermediate,
+                               const pdb::Handle<pdb::PDBSinkPageSetSpec> &sink,
+                               const pdb::Handle<pdb::Vector<pdb::Handle<pdb::PDBSourcePageSetSpec>>> &secondarySources,
+                               const bool swapLHSandRHS);
 
   ENABLE_DEEP_COPY
 
@@ -39,21 +39,59 @@ public:
   /**
    * //TODO
    */
-  bool setup(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage, Handle<pdb::ExJob> &job, const std::string &error) override;
+  bool setup(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage,
+             Handle<pdb::ExJob> &job,
+             const std::string &error) override;
 
   /**
    * //TODO
    */
   bool run(std::shared_ptr<pdb::PDBStorageManagerBackend> &storage) override;
 
-  void cleanup() override {  };
+  void cleanup() override;
 
-private:
+ private:
 
   /**
    * The intermediate page set
    */
   pdb::Handle<PDBSinkPageSetSpec> intermediate;
+
+  /**
+   * This forwards the preaggregated pages to this node
+   */
+  pdb::PDBPageSelfReceiverPtr selfReceiver;
+
+  /**
+   * These senders forward pages that are for other nodes
+   */
+  std::shared_ptr<std::vector<PDBPageNetworkSenderPtr>> senders;
+
+  /**
+   *
+   */
+  PDBLoggerPtr logger;
+
+  /**
+   * Vector of pipelines that will run this algorithm. The pipelines will be built when you call setup on this object.
+   * This must be null when sending this object.
+   */
+  std::shared_ptr<std::vector<PipelinePtr>> prebroadcastjoinPipelines = nullptr;
+
+  /**
+   *
+   */
+  std::shared_ptr<std::vector<PipelinePtr>> broadcastjoinPipelines = nullptr;
+
+  /**
+   *
+   */
+  std::shared_ptr<std::vector<PDBPageQueuePtr>> pageQueues = nullptr;
+
+  /**
+   *
+   */
+  LogicalPlanPtr logicalPlan;
 
   // mark the tests that are testing this algorithm
   FRIEND_TEST(TestPhysicalOptimizer, TestJoin1);
