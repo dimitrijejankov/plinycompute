7c738424b14a85787204e07c216187c262e84666
Vicram Rajagopalan <vicram.r@hotmail.com>
2019-07-02 16:54:34 -0500

    Removed the line that clones this repo, because obviously we would already have cloned the repo if we're calling the script

--- aws/setupAMI.sh
+++ aws/setupAMI.sh
@@ -2,13 +2,10 @@
 
 sudo apt install -y cmake
 
-git clone https://github.com/dimitrijejankov/plinycompute.git
-cd plinycompute
-
 # Python3 comes with Ubuntu Bionic: https://askubuntu.com/a/865569
 python3 scripts/internal/setupDependencies.py # 40-ish seconds
 
-# Install google benchmark. The below installation instructions
+# Build google benchmark from source. The below installation instructions
 # were found here: https://github.com/google/benchmark
 cd ..
 git clone https://github.com/google/benchmark.git
