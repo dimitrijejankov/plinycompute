450d71eacb569bfe749095dddd03bbb1f3846012
Vicram Rajagopalan <vicram.r@hotmail.com>
2019-07-07 14:52:27 -0500

    Renumbered command line args

--- aws/startWorker.sh
+++ aws/startWorker.sh
@@ -4,10 +4,10 @@
 # directory is the top-level plinycompute directory.
 
 # Usage:
-# ./aws/startWorker.sh workerPrivateIP managerPrivateIP
+# ./aws/startWorker.sh diskName workerPrivateIP managerPrivateIP
 #
 # Please see aws/README.md for details on how to find the instances' private IP addresses.
 
 ./aws/mountAndBuild.sh $1
 cd ../nvme # This way, the worker's data will be stored on the mounted NVME drive
-../plinycompute/bin/pdb-node -p 8109 -r ./pdbWorker1 -t 4 -i $1 -d $2
+../plinycompute/bin/pdb-node -p 8109 -r ./pdbWorker1 -t 4 -i $2 -d $3
