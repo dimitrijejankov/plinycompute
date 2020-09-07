e2d1e34e67994c0445d84d5178d3902a8f7055e1
dimitrijejankov <dimitrijejankov@gmail.com>
2020-09-07 16:46:32 -0500

    Added the index

--- pdb/src/executionServer/headers/physicalAlgorithms/TRABroadcastState.h
+++ pdb/src/executionServer/headers/physicalAlgorithms/TRABroadcastState.h
@@ -9,19 +9,11 @@ namespace pdb {
 
 struct TRABroadcastState : public PDBPhysicalAlgorithmState {
 
-  /**
-   * This forwards the preaggregated pages to this node
-   */
+
   pdb::PDBPageSelfReceiverPtr selfReceiver = nullptr;
 
-  /**
-   * These senders forward pages that are for other nodes
-   */
   std::shared_ptr<std::vector<PDBPageNetworkSenderPtr>> senders = nullptr;
 
-  /**
-   * The page queues
-   */
   std::shared_ptr<std::vector<PDBPageQueuePtr>> pageQueues = nullptr;
 
   PDBAbstractPageSetPtr inputSet = nullptr;
